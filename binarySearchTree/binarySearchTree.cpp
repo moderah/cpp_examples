4e72b4a6f93d7c5272a6213326dfe3046748889f
Brandon Autry <40213613+moderah@users.noreply.github.com>
2021-02-28 09:50:48 -0600

    Update binarySearchTree.cpp

--- binarySearchTree/binarySearchTree.cpp
+++ binarySearchTree/binarySearchTree.cpp
@@ -1,18 +1,3 @@
-// University of Arkansas at Little Rock
-// Department of Computer Science
-// CPSC 2380: Data Structures and Algorithms
-// Fall 2019
-// Project 2: Binary Search Tree
-// Due Date: November 26, 2019
-// Name:Brandon Autry		
-// T-number (Last 4 Digits): 5439
-// Description of the Program (2-3 sentences): 
-//	How I use a binary search tree to display and manipulate
-//	integer values. this can be translated from many different
-//	types of values.
-// Date Written: 11-14-2019
-// Date Revised: 11-26-2019
-
 
 #include "binarySearchTree.h"
 
